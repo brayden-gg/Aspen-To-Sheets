5b7b5bb2280710e55a6b38280a68198e6226bec0
Brayden Goldstein-Gelb <BraydensiMac@Braydens-iMac.local>
2020-03-24 15:15:27 -0400

    test

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.get('/update', (req, res) => {
 
 app.post('/update', (req, res) => {
 
+    console.log(req.body.username, req.body.email_address, req.body.spreadsheetId);
+
     updateGrades(req.body.username, req.body.password, req.body.email_address, req.body.spreadsheetId)
         .then(changes => res.send(JSON.stringify(changes)))
         .catch(err => console.log(err));
